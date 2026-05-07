0a9646564e688bdfe853661e9f6826bb5efc0b8b
Dory <78720364+dooory@users.noreply.github.com>
2026-05-07 14:41:59 +1200

    add caesarCipher function

--- myFunctions.js
+++ myFunctions.js
@@ -55,3 +55,55 @@ export const calculator = () => {
     mul,
   };
 };
+
+const characterCodeRanges = [
+  [123, 97], // Lowercase
+  [91, 65], // Uppercase
+  [58, 48], // Digits
+];
+
+const wrapNumber = (value, max, min) => {
+  const range = max - min;
+
+  const shifted = value - min;
+
+  const wrapped = ((shifted % range) + range) % range;
+
+  return wrapped + min;
+};
+
+const getCodeRanges = (code) => {
+  return (
+    characterCodeRanges.find((range) => code >= range[1] && code < range[0]) ||
+    []
+  );
+};
+
+const encryptCharacter = (ch, key) => {
+  const chCode = ch.charCodeAt(0);
+
+  const [maxCode, minCode] = getCodeRanges(chCode);
+
+  if (!maxCode || !minCode) {
+    return ch;
+  }
+
+  const adjustedChCode = wrapNumber(chCode + key, maxCode, minCode);
+  const encryptedCh = String.fromCharCode(adjustedChCode);
+
+  return encryptedCh;
+};
+
+export const caesarCipher = (string, key) => {
+  if (typeof string === "number") {
+    string = string.toString();
+  }
+
+  const splitString = string.split("");
+
+  const encryptedString = splitString
+    .map((ch) => encryptCharacter(ch, key))
+    .join("");
+
+  return encryptedString;
+};
---
+++ tests/caesarCipher.test.js
@@ -0,0 +1,18 @@
+import { caesarCipher } from "../myFunctions";
+
+test("Shift a string by shift factor", () => {
+  //   expect(caesarCipher("abc", 3)).toBe("def");
+  //   expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("xyz", -3)).toBe("uvw");
+  expect(caesarCipher("abc", -3)).toBe("xyz");
+
+  expect(caesarCipher("AbC", 3)).toBe("DeF");
+  expect(caesarCipher("AbC", -3)).toBe("XyZ");
+
+  expect(caesarCipher("HelloWorld", 3)).toBe("KhoorZruog");
+  expect(caesarCipher("Howdy, Hey!", 3)).toBe("Krzgb, Khb!");
+
+  expect(caesarCipher("My favorite number is 7", 3)).toBe(
+    "Pb idyrulwh qxpehu lv 0",
+  );
+});
